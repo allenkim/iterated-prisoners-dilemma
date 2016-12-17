7a3276cd0f97eb8ab711c8429f87fe1e2aa51542
Allen Kim <allenkim95@gmail.com>
2016-12-17 17:55:11 -0500

    Create working pd simulations

--- main.cpp
+++ main.cpp
@@ -6,13 +6,24 @@ using namespace std;
 
 typedef pair<int,int> ii;
 
+#define ALL_COOPERATE ((1<<16) - 1) // all 1's
+#define ALL_DEFECT 0
+
 int main(){
 	PayoffMatrix pmat;
-	Player allen(0);
-	Player porrith(0);
-	PD game(allen,porrith,pmat);
-	ii results = game.simulate();
-	cout << results.first << " " << results.second << endl;
+	Player allen(ALL_COOPERATE);
+	Player porrith(ALL_DEFECT);
+
+	// Tit for Tat - initially cooperate
+	// if enemy defects, you defect
+	// if enemy cooperates, you cooperate
+	Player tit_for_tat(0b1010101010101010);
+	PD game1(&allen,&tit_for_tat,&pmat);
+	ii results1 = game1.simulate(100);
+	cout << results1.first << " " << results1.second << endl;
+	PD game2(&porrith,&tit_for_tat,&pmat);
+	ii results2 = game2.simulate(100);
+	cout << results2.first << " " << results2.second << endl;
 
 	return 0;
 }
